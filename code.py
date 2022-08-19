ea05ff9e82c0ded8a868c6e5dac430b943f44097
AK232003 <76689824+AK232003@users.noreply.github.com>
2022-08-19 10:10:33 +0530

    Add files via upload
    Generalized the code for n-dimensional points

--- code.py
+++ code.py
@@ -1,47 +1,87 @@
-import numpy as np
+import time
 import math
-import plotly.express as px
+import psutil
 
+print("Enter maximum value of the element of vector:", end = ' ')
 n = int(input()) #max no of value in T1, T2, T3
 
-def Sum(l):
-    sum1=0
-    for i in l: sum1+=i
+print("Enter the dimension of vector:", end = ' ')
+d = int(input())
 
-    return sum1
-
-# inp = np.zeros([(n+1)**3,3], dtype='int') #populating list with [0, 0, 0]...
+start = time.time()
 ###################################################
 
+# l1 = []
 # l = []
-
-# for i in range(3*n+1):
+# z = 0
+# for i in range(d*n+1):
 #     for z in range(n+1):
 #         for y in range(n+1):
 #             for x in range(n+1):
 #                 if (x+y+z)==i :
-#                     l.append([x,y,z])
+#                     l1.append([x,y,z])
+#     l.append(l1)
+#     l1 = []
 # print(l)
+
+# max1 = int(input("Enter max Value: "))
+# dim = int(input("Enter Dimension: "))
+
+def sums(length, total_sum):
+    if length == 1:
+        yield (total_sum,)
+    else:
+        for value in range(total_sum + 1):
+            for permutation in sums(length - 1, total_sum - value):
+                yield (value,) + permutation
+
+l = []
+for i in range(0, n*d+1):
+    l1 = list(sums(d,i))
+    a = []
+    for j in l1:
+        if(max(j)>n): 
+            continue
+        else:
+            a.append(j)
+
+    T=[]
+    for i in a:
+        T.append(list(i))
+    T.reverse()
+    l.append(T)
 
 # ###################################################
 
-l = [[[0, 0, 0]], [[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[2, 0, 0], [1, 1, 0], [0, 2, 0], [1, 0, 1], [0, 1, 1], [0, 0, 2]], [[2, 1, 0], [1, 2, 0], [2, 0, 1], [1, 1, 1], [0, 2, 1], [1, 0, 2], [0, 1, 2]], [[2, 2, 0], [2, 1, 1], [1, 2, 1], [2, 0, 2], [1, 1, 2], [0, 2, 2]], [[2, 2, 1], [2, 1, 2], [1, 2, 2]], [[2, 2, 2]]]
-out = [[[0,0,[0,0,0]]],[[1,0,[0,0,1]], [0,1,[1,0,0]], [0.5, 0.5,[0,1,0]]]]
+# l = [[[0, 0, 0]], [[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[2, 0, 0], [1, 1, 0], [0, 2, 0], [1, 0, 1], [0, 1, 1], [0, 0, 2]], [[2, 1, 0], [1, 2, 0], [2, 0, 1], [1, 1, 1], [0, 2, 1], [1, 0, 2], [0, 1, 2]], [[2, 2, 0], [2, 1, 1], [1, 2, 1], [2, 0, 2], [1, 1, 2], [0, 2, 2]], [[2, 2, 1], [2, 1, 2], [1, 2, 2]], [[2, 2, 2]]]
+out = [[[0,0,l[0][0]]]] #base cases
+
+len1 = len(l[1])
+x1=0
+y1=1
+tempo = []
+r = 1/(len1-1)
+for i in l[1]:
+    x1+=r
+    y1-=r
+    tempo.append([x1,y1,i])
+out.append(tempo)
+
 
 def fn(c):
-    if(c<3): out.append([[c,0,l[c][-1]], [0,c,l[c][0]]])
-    else: out.append([[3,(c-3),l[c][-1]], [(c-3),3,l[c][0]]]) 
+    if(c<((d*n)/2)): out.append([[c,0,l[c][-1]], [0,c,l[c][0]]])
+    else: out.append([[(d*n)/2,(c-(d*n)/2),l[c][-1]], [(c-(d*n)/2),(d*n)/2,l[c][0]]]) 
     # print(c)
     temp = len(l[c])
     l[c].remove(l[c][-1])
     l[c].remove(l[c][0])
-    if(c<=3):
+    if(c<=((d*n)/2)):
         x = 0
         y = c
     else:
-        x = c-3
-        y = 3 
-    # print(l[c])+
+        x = c-((d*n)/2)
+        y = ((d*n)/2) 
+    # print(l[c])
     count = 1
     change = 0
     if(c%2==0): change+=(temp/2)
@@ -64,6 +104,7 @@ def fn(c):
                 temp1.append(i)
         if(len(temp1) > 1):
             temp2 = [temp1[0]]
+            print(p2, temp1[0])
             dis1 = math.dist(temp1[0], p2)
             for i in temp1:
                 if(dis1 > math.dist(i, p2)):
@@ -176,19 +217,16 @@ def fn(c):
             l[c].remove(temp1[0])
             # count+=1
 
-for i in range(2, 3*n):
+for i in range(2, d*n):
     fn(i)
-out.append([[(3*n)/2, (3*n)/2, [2,2,2]]])
-
-# print(out)
-xpos=[]
-ypos=[]
+out.append([[(d*n)/2, (d*n)/2, [n for i in range(d)]]])
 for i in out:
     for j in i:
         l1 = j[2]
         coords = [j[0],j[1]]
-        xpos.append(j[0])
-        ypos.append(j[1])
         print(str(l1)+": "+str(coords))
-fig = px.scatter(x=xpos, y=ypos, title='LD Points', color_discrete_sequence=['red'])
-fig.show()
+
+end = time.time()
+print("Time Elapsed(CPU): ", time.process_time())
+print("Time Elapsed(to run script): ", end-start)
+print('RAM memory used:', psutil.virtual_memory()[3]/(1024**2))
